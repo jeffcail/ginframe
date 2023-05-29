096b2db221d783b4acde12a32d45163d84b9b63d
cailj <cailj@anling.com.cn>
2023-05-29 18:04:39 +0800

    数组切片去重

---
+++ utils/slice/slice.go
@@ -0,0 +1,19 @@
+package _slice
+
+// RemoveRepeatedElement 数组切片去重
+func RemoveRepeatedElement(arr []string) (newArr []string) {
+	newArr = make([]string, 0)
+	for i := 0; i < len(arr); i++ {
+		repeat := false
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				repeat = true
+				break
+			}
+		}
+		if !repeat {
+			newArr = append(newArr, arr[i])
+		}
+	}
+	return
+}
